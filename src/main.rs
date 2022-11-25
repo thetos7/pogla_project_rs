054d5cccf8ab00ddad21d47c20f6af818493b375
Ancelin Bouchet <ancelin@bouchet.info>
2022-11-25 17:00:43 +0100

    feat: Added sdl window creation with opengl context

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate gl;
+extern crate sdl2;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let window = video_subsystem
+        .window("POGLA project", 1024, 1024)
+        .resizable()
+        .opengl()
+        .build()
+        .unwrap();
+    
+    let _gl_context = window.gl_create_context().unwrap();
+    
+    let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
+    unsafe {
+        gl::ClearColor(1.,0.,0.,1.);
+    }
+
+    let mut pump = sdl.event_pump().unwrap();
+    'main: loop {
+        for event in pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. } => break 'main,
+                _ => {}
+            }
+        }
+
+        unsafe{ gl::Clear(gl::COLOR_BUFFER_BIT) }
+
+        window.gl_swap_window();
+    }
 }
