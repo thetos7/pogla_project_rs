50ded0ddf24bb7ae33ff67818235437e87fb5933
Ancelin Bouchet <ancelin@bouchet.info>
2022-11-28 10:24:16 +0100

    fmt: formatted main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,25 +12,30 @@ fn main() {
         .opengl()
         .build()
         .unwrap();
-    
+
     let _gl_context = window.gl_create_context().unwrap();
-    
-    let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
+    let _gl =
+        gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
 
     unsafe {
-        gl::ClearColor(1.,0.,0.,1.);
+        gl::ClearColor(1., 0., 0., 1.);
     }
 
     let mut pump = sdl.event_pump().unwrap();
     'main: loop {
         for event in pump.poll_iter() {
             match event {
-                Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), .. }  => break 'main,
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'main,
                 _ => {}
             }
         }
 
-        unsafe{ gl::Clear(gl::COLOR_BUFFER_BIT) }
+        unsafe { gl::Clear(gl::COLOR_BUFFER_BIT) }
 
         window.gl_swap_window();
     }
