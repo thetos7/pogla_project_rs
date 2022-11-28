973f24977e474525936f4a3555c86955f52e6792
Ancelin Bouchet <ancelin@bouchet.info>
2022-11-28 10:52:15 +0100

    refactor: Swapped unwraps with expects

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,26 @@ fn clear_frame() {
 }
 
 fn main() {
-    let sdl = sdl2::init().unwrap();
-    let video_subsystem = sdl.video().unwrap();
+    let sdl = sdl2::init().expect("Couldn't initialize SDL2.");
+    let video_subsystem = sdl.video().expect("Couldn't create video system.");
     let window = video_subsystem
         .window("POGLA project", 1024, 1024)
         .resizable()
         .opengl()
         .build()
-        .unwrap();
+        .expect("Couldn't create window");
 
-    let _gl_context = window.gl_create_context().unwrap();
-
+    let _gl_context = window
+        .gl_create_context()
+        .expect("Couldn't create openGL context");
     let _gl =
         gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
 
     unsafe { gl::ClearColor(0.66, 0.66, 0.66, 1.) };
 
-    let mut pump = sdl.event_pump().unwrap();
+    let mut pump = sdl
+        .event_pump()
+        .expect("SDL Event pump creation failed, one may already exist.");
     'main: loop {
         for event in pump.poll_iter() {
             match event {
