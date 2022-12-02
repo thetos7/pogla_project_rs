2ece266f9e949f4623393c180e01bc5cec3b9d3a
Ancelin Bouchet <ancelin@bouchet.info>
2022-12-02 13:42:39 +0100

    cleanup: Removed unused code

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
+extern crate ansi_term;
 extern crate gl;
 extern crate sdl2;
-extern crate ansi_term;
 
 use engine::Engine;
-use sdl2::{event::Event, keyboard::Keycode};
 mod engine;
 mod gl_check;
-
-fn clear_frame() {}
-const HEIGHT: i32 = 1024;
-const WIDTH: i32 = 1024;
 
 fn main() {
     let engine = unsafe { Engine::instance() };
