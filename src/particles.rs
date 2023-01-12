73439629d312235b2f381ebad04f964f6b44701a
Ancelin Bouchet <ancelin@bouchet.info>
2023-01-12 19:42:54 +0100

    fix: Fixed particle passing to compute shader (?)

--- src/particles.rs
+++ src/particles.rs
@@ -4,7 +4,7 @@ use gl::types::GLfloat;
 
 use crate::{gl_checked, traits::ParticleLike};
 
-#[repr(C)]
+#[repr(C, align(16))]
 #[derive(Debug)]
 pub struct FireParticle {
     pub lifetime: GLfloat,
@@ -17,15 +17,18 @@ pub struct FireParticle {
 impl ParticleLike for FireParticle {
     fn setup_attributes() {
         let size = size_of::<Self>();
-        let float_size = size_of::<GLfloat>();
         unsafe {
             gl_checked! {
-                gl::VertexAttribPointer(1, 1, gl::FLOAT, gl::FALSE, size as _, (0 * float_size) as _);
-                gl::VertexAttribPointer(2, 1, gl::FLOAT, gl::FALSE, size as _, (1 * float_size) as _);
-                gl::VertexAttribPointer(3, 3, gl::FLOAT, gl::FALSE, size as _, (2 * float_size) as _);
+                gl::VertexAttribPointer(1, 1, gl::FLOAT, gl::FALSE, size as _, 0 as _);
+                gl::VertexAttribPointer(2, 1, gl::FLOAT, gl::FALSE, size as _, 4 as _);
+                gl::VertexAttribPointer(3, 3, gl::FLOAT, gl::FALSE, size as _, 8 as _);
+                gl::VertexAttribPointer(4, 1, gl::FLOAT, gl::FALSE, size as _, 20 as _);
+                gl::VertexAttribPointer(5, 3, gl::FLOAT, gl::FALSE, size as _, 24 as _);
                 gl::EnableVertexAttribArray(1);
                 gl::EnableVertexAttribArray(2);
                 gl::EnableVertexAttribArray(3);
+                gl::EnableVertexAttribArray(4);
+                gl::EnableVertexAttribArray(5);
             };
         }
     }
