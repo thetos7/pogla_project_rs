52f81e4bbc507d89eb1db282dd3b05f8efc143c1
Ancelin Bouchet <ancelin@bouchet.info>
2023-01-12 18:02:21 +0100

    feat: changed fire particle

--- src/particles.rs
+++ src/particles.rs
@@ -1,21 +1,32 @@
+use std::mem::size_of;
+
 use gl::types::GLfloat;
 
-use crate::traits::ParticleLike;
-
+use crate::{gl_checked, traits::ParticleLike};
 
 #[repr(C)]
-struct FireParticle {
-    lifetime: GLfloat,
-    scale: GLfloat,
-    rotation: GLfloat,
-    angular_velocity: GLfloat,
-    position: [GLfloat; 3],
-    velocity: [GLfloat; 3]
+#[derive(Debug)]
+pub struct FireParticle {
+    pub lifetime: GLfloat,
+    pub rotation: GLfloat,
+    pub position: [GLfloat; 3],
+    pub angular_velocity: GLfloat,
+    pub velocity: [GLfloat; 3],
 }
 
 impl ParticleLike for FireParticle {
     fn setup_attributes() {
-        todo!()
+        let size = size_of::<Self>();
+        let float_size = size_of::<GLfloat>();
+        unsafe {
+            gl_checked! {
+                gl::VertexAttribPointer(1, 1, gl::FLOAT, gl::FALSE, size as _, (0 * float_size) as _);
+                gl::VertexAttribPointer(2, 1, gl::FLOAT, gl::FALSE, size as _, (1 * float_size) as _);
+                gl::VertexAttribPointer(3, 3, gl::FLOAT, gl::FALSE, size as _, (2 * float_size) as _);
+                gl::EnableVertexAttribArray(1);
+                gl::EnableVertexAttribArray(2);
+                gl::EnableVertexAttribArray(3);
+            };
+        }
     }
 }
-
